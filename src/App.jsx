a55aeb1260120fac0f053850fdcf6f84e94db78b
Tushar Panwar <tusharpanwar0000@gmail.com>
2026-09-16 16:13:41 +0530

    Fixed login logic with !token check

--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,79 @@ import axios from 'axios'
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [token, setToken] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  const handleLogin = () => {
+    axios.post('http://localhost:3000/login', {
+      username: username,
+      password: password
+    })
+     .then(res => {
+     setToken(res.data.token)
+     }).catch(error => {
+      console.log('Login error', error)
+     });
+  }
+ 
   
   useEffect(() => {
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2YWE4ZDdkODBlZjQwZDE3ZGM3NmMwMTQiLCJ1c2VybmFtZSI6InR1c2hhcm1vbmdvIiwiaWF0IjoxNzg5NDUwNzg3LCJleHAiOjE3ODk0NTQzODd9.DmHmYOiA4gAz129v7Q-euH-0MOgYg7Iv0pOYS0nrqFg';
-    axios.get('http://localhost:3000/products', 
-      {
-        headers: {'Authorization': token}
-      }
-    )
+    if(token){
+
+
+      axios.get('http://localhost:3000/products', 
+        {
+          headers: {'Authorization': token}
+        }
+      )
+    
     .then(res => {
       setProducts(res.data)
     })
     .catch(error => {
       console.log(error)
     })
-  },[])
+  }
+  }, [token])
+
 
   return (
     <>
        <div>
         <h1>Tushar Shop</h1>
-        <ul>
-          {products.map((product) => (
-            <li key={product._id}>
-              {product.name} - ₹{product.price}
-            </li>
-          ))}
-        </ul>
+        { !token ?  (
+          <div>
+            <h2>Login karo</h2>
+            <input type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)} 
+            />
+            <input 
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={handleLogin}>Login</button>
+
+          </div>
+        ) :(
+          <div>
+            <h2>Products</h2>
+            <ul>
+              {products.map((product) => (
+                <li key={product._id}>
+                  {product.name} - ₹{product.price}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )
+
+        }
        </div>
     </>
   )
